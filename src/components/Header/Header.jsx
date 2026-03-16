69dfd72b07afd740bf2c532073b48eaf66170406
Ajayi-segun <segunajayi183@gmail.com>
2026-03-16 15:04:43 +0000

    fropdowns

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,24 @@ import SearchBar from "../searchBar/searchBar"
 import MenuIcon from "../../assets/MenuIcon.svg"
 import { useState } from "react"
 
+function NavDropdown({text, children}) {
+    const [isOpen, setIsOpen] = useState(false)
+
+
+// make the dropdown content only apper when isOpen is true
+
+    return(
+        <div className="nav-dropdown" onMouseLeave={() => setIsOpen(false)}>
+            <button className="nav-link" onMouseEnter={() => setIsOpen(true)}>{text}</button>
+            {
+            isOpen ? <div className="dropdown-content">
+                {children}
+                </div> :null
+            }
+        </div>
+    )
+}
+
 function DestopHeader(){
     return (
         <header className="main-header">
@@ -13,6 +31,10 @@ function DestopHeader(){
                 <SearchBar/>
             </div>
             <nav className="header-col">
+                <NavDropdown text="categories">
+                    <a className="nav-link" href="#">FASHION</a>
+                    <a className="nav-link" href="#">GARDENING</a>
+                </NavDropdown>
                 <a className="nav-link" href="#">Home</a>
                 <a className="nav-link" href="#">About</a>
                 <a className="nav-link" href="#">Product</a>
