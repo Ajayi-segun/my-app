319a5b6f641ee7a337094904d18626fa20e6a129
Ajayi-segun <segunajayi183@gmail.com>
2026-03-16 11:59:30 +0000

    Toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import "./Header.css"
 import SearchBar from "../searchBar/searchBar"
-export default function Header() {
+import MenuIcon from "../../assets/MenuIcon.svg"
+import { useState } from "react"
+
+function DestopHeader(){
     return (
         <header className="main-header">
             <div className="header-col">
@@ -15,5 +18,38 @@ export default function Header() {
                 <a className="nav-link" href="#">Product</a>
             </nav>
         </header>
+    )
+}
+
+function MobileHeader() {
+    const [isOpen, setIsOpen] = useState(false)
+    function toggleOpen() {
+        setIsOpen(!isOpen);
+    }
+    return(
+        <div className="mobile-header">
+            <div className="logo">LogoText</div>
+            <button className="menu-btn" onClick={toggleOpen}>
+                <img width="40" src={MenuIcon} alt="menu icon"/>
+            </button>
+            {
+            isOpen ? <nav className="mobile-nav">
+                <SearchBar/>
+                <a className="nav-link" href="#">Home</a>
+                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#">Product</a>
+            </nav> : null
+            }
+        </div>
+
+    )
+}
+export default function Header() {
+    return (
+        <>
+    <DestopHeader/>
+    <MobileHeader/>
+    </>
+      
     )
 }
